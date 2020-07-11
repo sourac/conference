4d68a2a776666fb7ba66e728c13a6d07a1f2639a
Atul Sharma <atulsharma21772@gmail.com>
2020-07-11 16:47:05 +0530

    Signed-off-by: Atul Sharma <atulsharma21772@gmail.com>

--- src/main/java/com/pluralsight/repository/HibernateSpeakerRepositoryImpl.java
+++ src/main/java/com/pluralsight/repository/HibernateSpeakerRepositoryImpl.java
@@ -5,8 +5,9 @@ import com.pluralsight.model.Speaker;
 import java.util.ArrayList;
 import java.util.List;
 
-public class HibernateSpeakerRepositoryImpl {
+public class HibernateSpeakerRepositoryImpl implements SpeakerRepository {
 
+    @Override
     public List<Speaker> findAll(){
 
         List<Speaker> speakers=new ArrayList<>();
